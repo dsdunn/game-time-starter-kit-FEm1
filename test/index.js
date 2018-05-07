d97e7b82f7b74fa996db8a4e52b50b6e79e21549
dsdunn <dsdunnmusic@gmail.com>
2018-05-07 16:21:16 -0600

    Finish tests, move gameloop to index, create snake.draw

--- test/index.js
+++ test/index.js
@@ -1,21 +1,98 @@
 const { expect } = require('chai');
-const index = require('../lib/index');
-const block = require('../lib/block.js');
-const snake = require('../lib/snake.js');
-const target = require('../lib/target.js')
+const Game = require('../lib/game.js')
+const Block = require('../lib/block.js');
+const Snake = require('../lib/snake.js');
+const Target = require('../lib/target.js')
 
 describe('Block', () => {
 
-  let block = new Block();
+  let block = new Block(55, 65);
 
-  it('has a height and width of 15 pixels', () => {
-    expect(block.width).to.equal(15);
-    expect(block.height).to.equal(15);
+  it('has a height and width of 20 pixels', () => {
+    expect(block.width).to.equal(20);
+    expect(block.height).to.equal(20);
+  })
+
+  it('takes x and y coordinates', () => {
+    expect(block.x).to.equal(55);
+    expect(block.y).to.equal(65);
   })
 })
 
+describe('Target', () => {
+  const target = new Target();
+  const target1 = new Target();
 
-// describe('Head', () => {
-//   let head = new Head();
+  it('should extend Block', () => {
+    expect(target).to.be.an.instanceof(Block);
+  })
 
-// })
+  it('should generate random x and y coordinates', () => {
+    expect(target.x).to.not.equal(target1.x);
+    expect(target.y).to.not.equal(target1.y);
+  })
+})
+
+describe('Snake', () => {
+  const game = new Game();
+  const snake = game.snake;
+
+  it('should instantiate Snake', () => {
+    expect(snake).to.be.an.instanceof(Snake);
+  })
+
+  it('should have a body', () => {
+    expect(snake.body).to.deep.equal([]);
+  })
+
+  it('should have a default direction of "right"', () => {
+    expect(snake.direction).to.equal('right');
+  })
+
+  it('should fill its body with segments', () => {
+    snake.createSnake();
+    expect(game.snake.body.length).to.equal(5);
+  })
+
+  it('should be able to move', () => {
+    const initialHeadPosition = snake.body[4];
+    snake.move();
+    expect(snake.body[4].x).to.equal(initialHeadPosition.x + 20);
+  })
+
+  it('should be able to change direction', () => {
+    const initialHeadPosition = snake.body[4];
+    snake.direction = 'up';
+    snake.move();
+    expect(snake.body[4].y).to.equal(initialHeadPosition.y - 20);
+  })
+
+  it('should be able to grow', () => {
+    const initialBodyLength = snake.body.length;
+    snake.grow();
+    expect(snake.body.length).to.equal(initialBodyLength + 1);
+  })
+})
+
+describe('Game', () => {
+  const game = new Game();
+
+  it('should instantiate a new game, snake, and target', () => {
+    expect(game).to.be.an.instanceof(Game);
+    expect(game.snake).to.be.an.instanceof(Snake);
+    expect(game.target).to.be.an.instanceof(Target);
+  })
+
+  it('should detect when snake collides', () => {
+    game.snake.createSnake();
+    game.snake.move();
+    game.collisionDetection();
+    expect(game.score).to.equal(0);
+    game.snake.head.x = game.target.x;
+    game.snake.head.y = game.target.y;
+    game.collisionDetection();
+    expect(game.score).to.equal(100);
+  })
+
+})
+
